b47a1a0e933a93f8a9a4866fb1a571f1c12c9b77
dobos <doboszdav@gmail.com>
2024-04-21 02:21:30 +0200

    cc

---
+++ src/main/java/ee/pw/hackathon/besthackingleagueproject/dto/input/SearchFiltersInputRequest.java
@@ -0,0 +1,15 @@
+package ee.pw.hackathon.besthackingleagueproject.dto.input;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter@NoArgsConstructor@AllArgsConstructor
+@Builder
+public class SearchFiltersInputRequest {
+  private SearchFiltersInput searchFiltersInput;
+
+}
